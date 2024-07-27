898934194210748dd4d99de94dd5d8fe085725ee
yenling <yenling99992@gmail.com>
2024-07-27 15:49:16 +0800

    output對齊

--- Data_Types.cpp
+++ Data_Types.cpp
@@ -16,10 +16,10 @@ int main(){
     float f = 0.123456f;
     char c = 'c';
 
-    printf("int i = %d\t size of i = %zu\n", i, sizeof(i));
+    printf("int i = %d\t\t\t size of i = %zu\n", i, sizeof(i));
     printf("double d = %lf\t size of i = %zu\n", d, sizeof(d));
-    printf("float f = %.2f\t size of i = %zu\n", f, sizeof(f));//.2f 表示取到小數點第2位
-    printf("char c = %c\t size of i = %zu\n", c), sizeof(c);
+    printf("float f = %.2f\t\t size of i = %zu\n", f, sizeof(f));//.2f 表示取到小數點第2位
+    printf("char c = %c\t\t\t size of i = %zu\n", c), sizeof(c);
 
     return 0;
 }
