92febb418eaf30a3e7cdf30430bbe3e5d0e37c56
yenling <yenling99992@gmail.com>
2024-07-28 22:03:48 +0800

    Types Conversation

---
+++ 7_Types_Conversation.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    char c = '5';//ascii = 53
+    double a = 5.67;
+    int b = 9;
+    int result_int = a + b;
+    double result_double = a + b;
+    int result_int_2 = (int)a + b;
+    double result_double_2 = (int)a + b;
+
+    printf("c = %c\ta = %f\tb = %d\n", c, a, b);
+
+    printf("c + b = %d\n", c + b);
+    printf("a + b <in int result> = %d\n", result_int);
+    printf("a + b <in double result> = %lf\n", result_double);
+    printf("(int)a + b <in int result>= %d\n", result_int_2);
+    printf("(int)a + b <in double result>= %lf\n", result_double_2);
+
+
+    return 0;
+}
