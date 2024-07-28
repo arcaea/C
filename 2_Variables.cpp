2c76ba925a28247e77cb6aaf22b1001daf680d9c
yenling <yenling99992@gmail.com>
2024-07-28 21:29:30 +0800

    rename

---
+++ 2_Variables.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 10;
+    printf("first n:%d\n", n);
+
+    n = 20;
+    printf("Second n:%d\n", n);
+
+    int new_n = n;
+    printf("new n:%d\n", new_n);
+
+    int var1, var2 = 30;
+    printf("var1:%d\tvar2:%d\n", var1, var2);
+
+    return 0;
+}
---
+++ 4_Inputs.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int intsc, multisc1;
+    double doublesc, multisc2;
+    char charsc;
+
+    printf("plz enter int:"); 
+    scanf("%d",&intsc);
+
+    printf("plz enter double:"); 
+    scanf("%lf",&doublesc);
+
+    printf("plz enter char:"); 
+    scanf("%c",&charsc);
+    
+    printf("plz enter multi var(int, double):"); 
+    scanf("%d %lf",&multisc1, &multisc2);
+
+    printf("int inputs = %d\n",intsc);
+    printf("double inputs = %lf\n",doublesc);
+    printf("char inputs = %c\n",charsc);
+    printf("multi var inputs = %d , %lf\n", multisc1, multisc2);
+
+    return 0;
+}
