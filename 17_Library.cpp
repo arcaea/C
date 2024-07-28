3b6d22268d112d2d467c21d040cc559593653048
yenling <yenling99992@gmail.com>
2024-07-28 23:25:58 +0800

    Library

---
+++ 17_Library.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>//輸入輸出提供的函式庫
+#include <math.h>// 數學函數提供的函式庫
+#include <ctype.h>// 字元函數提供的函式庫
+
+
+int main(){
+
+    printf("Squre root(sqrt(4)): %lf\n",sqrt(4));
+    printf("Power(pow(5,2)): %lf\n",pow(5,2));
+
+    printf("upper char 'a': %c\n",toupper('a'));
+
+    return 0;
+}
