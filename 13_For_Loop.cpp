cdfefc31eae805c1a17c0c6280374cd59c3b1bbf
yenling <yenling99992@gmail.com>
2024-07-28 22:53:19 +0800

    for loop

---
+++ 13_For_Loop.cpp
@@ -0,0 +1,15 @@
+/*
+for(起始值;條件;更新式){
+    //code inside the for loop
+}
+*/
+#include <stdio.h>
+
+int main(){
+
+    for(int a = 0;a <= 10;a++){
+        printf("a = %d\n",  a);
+    }
+
+    return 0;
+}
