43474a9c0c7a5b185bd312a1cd3f6aeeccf4fe1a
yenling <yenling99992@gmail.com>
2024-07-28 23:49:47 +0800

    Recursion

---
+++ 18_Recursion.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int i){
+   if (i != 0)
+    return i + sum(i-1);
+   else
+    return i;
+}
+
+int main(){
+
+    int result = sum(10);
+
+    printf("sum 0 to 10 = %d", result);
+
+    return 0;
+}
